21420c8affea99f86b1479bb6a8d75364405c73c
Sriya Rao Muthyala <148452646+sriyaraomuthyala@users.noreply.github.com>
2025-02-17 17:28:41 +0530

    Delete streams directory

--- streams/ex-1-5.py
+++
@@ -1,8 +0,0 @@
-import itertools
-def process_iterator(iterator, func):
-    for item in iterator:
-        yield func(item)
-
-if __name__ == "__main__":
-    for num in process_iterator(range(1, 6), lambda x: x * 2):
-        print(num)
