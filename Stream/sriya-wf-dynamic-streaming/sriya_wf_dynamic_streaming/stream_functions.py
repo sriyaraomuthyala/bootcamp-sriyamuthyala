453f1c2802e2d0a49ce26438b30770540069ed68
Sriya Rao Muthyala <148452646+sriyaraomuthyala@users.noreply.github.com>
2025-02-18 17:31:30 +0530

    Delete Stream directory

--- Stream/sriya-wf-dynamic-streaming/sriya_wf_dynamic_streaming/stream_functions.py
+++
@@ -1,6 +0,0 @@
-def upper_case(text: str) -> str:
-    return text.upper()
-def capitalize(text: str) -> str:
-    """Capitalize the first letter of each word in a sentence and ensure a newline at the end."""
-    return " ".join(word.capitalize() for word in text.split()) + "\n"
-
