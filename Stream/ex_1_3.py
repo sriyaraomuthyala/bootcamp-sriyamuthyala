98a4c5381439194bcab13f2667da76eb6295d361
Sriya Rao Muthyala <148452646+sriyaraomuthyala@users.noreply.github.com>
2025-02-18 16:15:43 +0530

    Delete Stream directory

--- Stream/ex_1_3.py
+++
@@ -1,9 +0,0 @@
-def file_generator(filename):
-    with open(filename, 'r') as file:
-        for line in file:
-            yield line.strip()
-
-if __name__ == "__main__":
-    filename = "sample.txt"
-    for line in file_generator(filename):
-        print(line)
