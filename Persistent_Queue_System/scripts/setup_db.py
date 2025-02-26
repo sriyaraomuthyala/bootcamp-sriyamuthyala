ebfc0e70a58e137bbcf4275373398716c6a2b370
Sriya Rao Muthyala <148452646+sriyaraomuthyala@users.noreply.github.com>
2025-02-26 21:08:10 +0530

    Update setup_db.py

--- Persistent_Queue_System/scripts/setup_db.py
+++ Persistent_Queue_System/scripts/setup_db.py
@@ -1,14 +1,20 @@
 import sqlite3
 
-conn = sqlite3.connect("queue.db")
+DB_FILE = "queue.db"
+
+conn = sqlite3.connect(DB_FILE)
 cursor = conn.cursor()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS queue (
-    job_id TEXT PRIMARY KEY,
-    job_data TEXT,
-    status TEXT DEFAULT 'pending'
-)
+    CREATE TABLE IF NOT EXISTS queue (
+        job_id TEXT PRIMARY KEY,
+        job_data TEXT NOT NULL,
+        status TEXT NOT NULL DEFAULT 'pending',
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
 """)
+
 conn.commit()
 conn.close()
+
+print("✅ Database setup completed.")
